98167ac23651c3ee7f02741dee6454478fa26cd4
tanner-andrulis <andrulis@mit.edu>
2026-01-05 11:32:05 -0500

    Fix pip install from git repo

--- hwcomponents/_version.py
+++ hwcomponents/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '5.0.6'
-__version_tuple__ = version_tuple = (5, 0, 6)
+__version__ = version = '5.0.7'
+__version_tuple__ = version_tuple = (5, 0, 7)
 
-__commit_id__ = commit_id = 'gf0acbcb70'
+__commit_id__ = commit_id = 'ga2a56804b'
